e3297cb07cf24fc3b2f60f06a0d151b8633e8a1e
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-08-31 13:58:24 +0900

    [Feat] 바닥 오브젝트 추가

--- unity_1/Assets/Player.cs
+++ unity_1/Assets/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("�÷��̾� Start");
+        Debug.Log("플레이어 Start");
+        Debug.Log("IDE 변경확인용");
     }
 
     
