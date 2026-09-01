c0c444be1383750b22664fd183597225ab0f5fc3
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-01 12:32:06 +0900

    [Feat]점프 구현 및 초기화
    다음 수업 진행을 위해 점프 구현까지 남기고 코드 정리

--- 260901/Assets/Scenes/NewBehaviourScript.cs
+++ 260901/Assets/Scenes/NewBehaviourScript.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    
-    [Header("회전 관련 데이터")]
-    [Tooltip("회전 관련 데이터를 설정합니다")]
-    [SerializeField] [Range(0, 300)] private float _rotSpeed;
+   
 
     public float JumpForce;
     public Rigidbody _rigidbody;
     
-    [field:SerializeField]public float MoveSpeed { get; private set; }
-    public Vector3 _tempVector;
-
+  
     
     private void Update()
     {
@@ -25,7 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
         
         
         
-        transform.Rotate(Vector3.up * _rotSpeed * Time.deltaTime);
+       
 
     }
 }
