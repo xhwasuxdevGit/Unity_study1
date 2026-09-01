a5939c5208eedf0ff43de4ff8113e01c5d961e65
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-01 15:04:32 +0900

    [Feat] 디버그 로그 출력 추가

--- 260901/Assets/Scenes/VisibilitySwitch.cs
+++ 260901/Assets/Scenes/VisibilitySwitch.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VisibilitySwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const string TAG_FAR = "FarTarget";
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Renderer _childRenderer;
+
+    [SerializeField] private bool _showSelf = true;
+    [SerializeField] private bool _showChild = true;
+    [SerializeField] private bool _showFar = true;
+
+    private Renderer _selfRenderer;
+    private Renderer _farRenderer;
+
+    private void Awake()
     {
+        _selfRenderer = GetComponent<Renderer>();
+
+        GameObject farObject = GameObject.FindWithTag(TAG_FAR);
+        _farRenderer = farObject.GetComponent<Renderer>();
+        
+        Debug.Log($"VisivilitySwitch: 자신은 {_selfRenderer.name}입니다.");
+        Debug.Log($"VisivilitySwitch: 연결된 자식은 {_childRenderer.name}입니다.");
+        Debug.Log($"VisivilitySwitch: 태그로 찾은 것은 {_farRenderer.name}입니다.");
+
+        Renderer found = GetComponentInChildren<Renderer>();
+        Debug.Log($"VisibilitySwitch: GetComponentInChildren이 돌려준 것은 {found.name}입니다.");
         
     }
 }
