cff53a010f373504451e527210e244a9b5fe5a56
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-02 13:23:14 +0900

    [feat] 목표를 따라 정속으로 움직이기

---
+++ 260902/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+        
+    }
+    
+}
