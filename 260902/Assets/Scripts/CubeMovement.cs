597c1ef0380dd06c1b4f2fea776cc20a7b0bebc2
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-02 14:59:22 +0900

    Revert "[Feat] 이동하는 방향으로 회전하기 구현"
    This reverts commit 4cde2b602e92629ef98fd2366e0b358c5b7f5c8e.

--- 260902/Assets/Scripts/CubeMovement.cs
+++ 260902/Assets/Scripts/CubeMovement.cs
@@ -6,70 +6,33 @@ public class CubeMovement : MonoBehaviour
   
   [SerializeField] private float _moveSpeed;
   [SerializeField] private float _rotateSpeed;
- 
+  [SerializeField] private Transform _target;
   
   private void Update()
   {
-    Vector3 movement = GetMovement();
-    Move(movement);
+    Move();
     //MoveTotarget();
-    CubeRotate(movement);
-    
-    Vector3.Distance()
+    CubeRotate();
   }
 
-  private void CubeRotate(Vector3 movement)
+  private void CubeRotate()
   {
-    if (movement == Vector3.zero)
+    if (Input.GetKey(KeyCode.R))
     {
-      return;
+      transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime);
     }
-    
-    Quaternion look = Quaternion.LookRotation(movement);
-
-    transform.rotation = Quaternion.Slerp(
-      transform.rotation,
-      look,
-      _rotateSpeed * Time.deltaTime
-    );
-
-  }
-  
-  private void Move(Vector3 movement)
-  {
-    if (movement == Vector3.zero)
-    {
-      return;
-    }
-
-    
-   
-    transform.Translate(
-      Vector3.forward  * _moveSpeed * Time.deltaTime
-      );
-
-  }
-
-  private Vector3 GetMovement()
-  {
-    float x = Input.GetAxisRaw("Horizontal");
-    float z = Input.GetAxisRaw("Vertical");
-    
-    Vector3 movement = new Vector3(x, 0, z);
-    return movement.normalized;
-    // movement.normalized; normalize 대문자와 소문자는 다른 함수! 내용확인하자
   }
 
   private void MoveTotarget()
   {
-    /*
+
     transform.position = Vector3.Lerp(
       transform.position,
       _target.position,
       _moveSpeed * Time.deltaTime
     );
-  
- 
+
+    /*
     transform.position = Vector3.MoveTowards(
       transform.position, _target.position,
       _moveSpeed * Time.deltaTime
@@ -77,6 +40,20 @@ public class CubeMovement : MonoBehaviour
     */
   }
 
+  private void Move()
+  {
+    float x = Input.GetAxisRaw("Horizontal");
+    float z = Input.GetAxisRaw("Vertical");
+    
+    Vector3 movement = new Vector3(x, 0, z);
+    movement.Normalize();
+    // movement.normalized; normalize 대문자와 소문자는 다른 함수! 내용확인하자
+
+    //transform.position += movement * _moveSpeed * Time.deltaTime;
+    transform.Translate(movement * _moveSpeed * Time.deltaTime);
+
+
+  }
   
   
 }
