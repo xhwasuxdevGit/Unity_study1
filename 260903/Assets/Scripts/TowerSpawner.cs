56ecf2b318e3a12def65300d7600d4840b59adc9
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-03 15:59:24 +0900

    생성과 파괴 실습: 더 해 보기
    마지막 더해보기는 수업시간에 진행한 'PlayerSpawner.cs'을 참고함

--- 260903/Assets/Scripts/TowerSpawner.cs
+++ 260903/Assets/Scripts/TowerSpawner.cs
@@ -20,23 +20,31 @@ public class TowerSpawner : MonoBehaviour
       {
          SpawnOne();
       }
+
+      if (Input.GetKeyDown(KeyCode.D) && clones.Count != 0)
+      {
+         for (int i = 0; i < clones.Count; i++)
+         {
+            Destroy(clones[i]);
+         }
+        
+      }
    }
 
    private void SpawnOne()
    {
       
       Vector3 spawnPosition = new Vector3(_count * 2f, 0f, 0f);
-      for (int i = 0; i < clones.Count; i++)
-      { clones.Add(Instantiate(_towerPrefab,
-            spawnPosition, Quaternion.identity, _spawnRoot));
-         clones[i].name = "망루";
-         clones[i].AddComponent<TowerLifeLog>();
-         Destroy(clones[i], 2f);
-         Debug.Log($"TowerSpawner: 방금 없애라고 한 {clones[i].name}이 아직 여기 있습니다.");
-         _count += 1;
-         Debug.Log("TowerSpawner: {_count}번째를 만들었고 2초 뒤에 없앱니다.");
-      }
-     
+      GameObject clone = Instantiate(_towerPrefab,
+         spawnPosition, Quaternion.identity, _spawnRoot);
+      clone.name = "망루";
+      clone.AddComponent<TowerLifeLog>();
+      clones.Add(clone);
+      Debug.Log($"TowerSpawner: 방금 없애라고 한 {clone.name}이 아직 여기 있습니다.");
+     _count += 1;
+      
       
    }
+     
+   
 }
