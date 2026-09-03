dff460fa3fae1252e840fd3bfb352258f129957a
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-03 15:38:41 +0900

    Update TowerSpawner.cs

--- 260903/Assets/Scripts/TowerSpawner.cs
+++ 260903/Assets/Scripts/TowerSpawner.cs
@@ -7,6 +7,7 @@ public class TowerSpawner : MonoBehaviour
    [SerializeField] private GameObject _towerPrefab;
    private int _count;
    [SerializeField] private Transform _spawnRoot;
+   private List<GameObject> clones = new List<GameObject>();
 
    private void Update()
    {
@@ -25,13 +26,17 @@ public class TowerSpawner : MonoBehaviour
    {
       
       Vector3 spawnPosition = new Vector3(_count * 2f, 0f, 0f);
-      GameObject clone = Instantiate(
-         _towerPrefab, spawnPosition, Quaternion.identity, _spawnRoot);
-      clone.AddComponent<TowerLifeLog>();
-      Destroy(clone, 4f);
-      Debug.Log($"TowerSpawner: 방금 없애라고 한 {clone.name}이 아직 여기 있습니다.");
-     _count += 1;
-      Debug.Log("TowerSpawner: {_count}번째를 만들었고 2초 뒤에 없앱니다.");
+      for (int i = 0; i < clones.Count; i++)
+      { clones.Add(Instantiate(_towerPrefab,
+            spawnPosition, Quaternion.identity, _spawnRoot));
+         clones[i].name = "망루";
+         clones[i].AddComponent<TowerLifeLog>();
+         Destroy(clones[i], 2f);
+         Debug.Log($"TowerSpawner: 방금 없애라고 한 {clones[i].name}이 아직 여기 있습니다.");
+         _count += 1;
+         Debug.Log("TowerSpawner: {_count}번째를 만들었고 2초 뒤에 없앱니다.");
+      }
+     
       
    }
 }
