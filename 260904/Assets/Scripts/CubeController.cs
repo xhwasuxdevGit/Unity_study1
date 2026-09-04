16cf44ce209cfb6db54cf526e852eab2a85738f3
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-04 15:58:48 +0900

    Update CubeController.cs

--- 260904/Assets/Scripts/CubeController.cs
+++ 260904/Assets/Scripts/CubeController.cs
@@ -21,12 +21,30 @@ public class CubeController : MonoBehaviour
       if (Physics.Raycast(ray, out hit))
       {
          Debug.Log(hit.transform.name);
+
+      if (!Input.GetKeyDown(KeyCode.Space)) return;
+      
+      Ray ray = new Ray(transform.position, transform.forward);
+
+      RaycastHit[] hits = Physics.RaycastAll(ray, 10f);
+
+      if (hits.Length > 0)
+      {
+         foreach (RaycastHit hit in hits)
+         {
+            Debug.Log(hit.transform.name);
+         }
+
       }
    }
 
    private void OnDrawGizmos()
    {
       Gizmos.color = Color.green;
+
       Gizmos.DrawRay(transform.position, transform.forward * 5);
+
+      Gizmos.DrawRay(transform.position, transform.forward*10);
+
    }
 }
