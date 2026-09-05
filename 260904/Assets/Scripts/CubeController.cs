2b5dbcd4051cb5d6f1c1190ffd6a096d13fa5e18
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-09-05 16:40:43 +0900

    에셋 복구

---
+++ 260904/Assets/Scripts/CubeController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CubeController : MonoBehaviour
+{
+   private Camera _cam;
+   [SerializeField] private UnitMovement _target;
+
+   private void Start()
+   {
+      _cam = Camera.main;
+   }
+   
+   
+   private void Update()
+   {
+      RayShot();
+      MoveTarget();
+   }
+
+   private void RayShot()
+   {
+      if (!Input.GetMouseButtonDown(0)) return;
+       
+      Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+
+      if (Physics.Raycast(ray, out hit))
+      {
+         if (hit.transform.CompareTag("Ground"))
+         {
+            _target = null;
+            return;
+              
+         }
+         Debug.Log($"{hit.transform.name} 선택");
+         _target = hit.transform.GetComponent<UnitMovement>();
+           
+      }
+      else
+      {
+         _target = null;
+         return;   
+      }
+
+   }
+
+
+   private void MoveTarget()
+      {
+         if (!Input.GetMouseButtonDown(1) || _target == null) return;
+       
+         Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+         RaycastHit hit;
+
+         if (Physics.Raycast(ray, out hit))
+         {
+            if (!hit.transform.CompareTag("Ground")) return;
+
+            _target.SetDestination(hit.point);
+            return;
+         }
+      }
+}
---
+++ 260904/Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour
+{
+    private Vector4 _destination;
+    private bool _isMoving;
+    [SerializeField] private float _moveSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!_isMoving) return;
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            _destination,
+            _moveSpeed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, _destination) < 0.1f)
+        {
+            _isMoving = false;
+        }
+    }
+   
+    public void SetDestination(Vector3 destination)
+    {
+        _destination = destination;
+        _isMoving = true;
+    }
+}
